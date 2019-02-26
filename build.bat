1b37b7b56f1e1e7da28db1436c32f24e098bf151
Jayson Armbruster <Armbru43@msu.edu>
2019-02-25 22:56:20 -0500

    added assets and /docs build folder

---
+++ build.bat
@@ -0,0 +1,3 @@
+echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io/kiNG when files are comitted.
+ng build --deployUrl=jbru95.github.io/kiNG --outputPath=./docs
+PAUSE
