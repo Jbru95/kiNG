d61306ca3fd1d3df0f56c26121f6818acc7d0edd
Jayson Armbruster <Armbru43@msu.edu>
2019-02-25 23:40:37 -0500

    build 3

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 echo Building app in production mode, sending files to docs folder in repository, which is set to deploy automatically to jbru95.github.io/kiNG when files are comitted.
-ng build --deployUrl=jbru95.github.io/kiNG --outputPath=./docs
+ng build --prod --deployUrl="https://jbru95.github.io/kiNG/" --outputPath=./docs --base-href="https://jbru95.github.io/kiNG/"
 PAUSE
